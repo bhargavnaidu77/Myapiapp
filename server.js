ffcd64d9713af662d223b1bdfc8ade643be1b066
bhargavnaidu77 <120448254+bhargavnaidu77@users.noreply.github.com>
2023-11-12 21:42:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require("cors");
+const Registeruser=require("./model")
+const jwt=require("jsonwebtoken");
+const middleware=require("./middleware");
+
+const app=express();
+app.use(express.json());
+app.use(cors({origin:"*"}));
+
+MONGO_URL=process.env.MONGO_URL
+PORT=process.env.PORT
+mongoose.connect(MONGO_URL).then(()=>{
+    console.log("DB connected...")
+}).catch(()=>console.log("error occured db not connected"))
+app.post("/register", async(req,res)=>{
+    try{
+        const {username,email,password,confirmpassword}=req.body;
+        let exist=await Registeruser.findOne({email});
+        if(exist){
+            return res.status(400).send("User Already Exist");
+        }
+        if(password!==confirmpassword){
+            return res.status(400).send("Passwords are not matching");
+        }
+        let newUser= new Registeruser({
+            username,
+            email,
+            password,
+            confirmpassword
+        })
+        await newUser.save();
+        res.status(200).send("Registered Successfully");
+
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).send("Server Error");
+    }
+})
+
+app.post("/login", async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        let exist=await Registeruser.findOne({email});
+        if(!exist){
+            return res.status(400).send("User Not Found");
+        }
+        if(exist.password!==password){
+            return res.status(400).send("Invalid Credentials");
+        }
+        let payload={
+            user:{
+                id:exist.id
+            }
+        }
+        jwt.sign(payload,"tbk",{expiresIn:3600000},
+        (err,token)=>{
+            if(err) throw err;
+            return res.json({token})
+        })
+        
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Server Error");
+    }
+
+})
+
+app.get("/myprofile",middleware,async (req,res)=>{
+    try {
+        let exist= await Registeruser.findById(req.user.id);
+        if(!exist){
+            return res.status(400).send("User not found");
+        }
+        res.json(exist);
+
+        
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Server Error");
+    }
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT}....`)
+})
